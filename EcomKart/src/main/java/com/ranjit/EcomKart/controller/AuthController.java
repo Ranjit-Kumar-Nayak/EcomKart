81107cc87f179acf722a4125ed541204165d2ef7
ranjit <ranjitkumarnayak420@gmail.com>
2025-04-05 08:46:13 +0530

    AuthControllers

--- EcomKart/src/main/java/com/ranjit/EcomKart/controller/AuthController.java
+++ EcomKart/src/main/java/com/ranjit/EcomKart/controller/AuthController.java
@@ -1,7 +1,9 @@
 package com.ranjit.EcomKart.controller;
 
+import com.ranjit.EcomKart.Repositery.UserRepositery;
 import com.ranjit.EcomKart.model.User;
 import com.ranjit.EcomKart.response.SignUpRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,13 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final UserRepositery userRepositery;
     @PostMapping("/signup")
     public ResponseEntity<User> createUserHandler(@RequestBody SignUpRequest request){
 
         User user= new User();
         user.setEmail(request.getEmail());
         user.setFullname(request.getFullname());
+        userRepositery.save(user);
         return ResponseEntity.ok(user);
 
     }
