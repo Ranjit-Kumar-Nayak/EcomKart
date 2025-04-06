e0e5065b92d85b9fd7fee9604eddcb442f94e98c
ranjit <ranjitkumarnayak420@gmail.com>
2025-04-07 01:41:44 +0530

    jwt and security config

---
+++ EcomKart/src/main/java/com/ranjit/EcomKart/config/JWT_CONSTANT.java
@@ -0,0 +1,6 @@
+package com.ranjit.EcomKart.config;
+
+public class JWT_CONSTANT {
+    public static  final  String SCRECT_KEY="jhjfhghgchchcgcggcg";
+    public static  final String JWT_HEADER="Authorization";
+}
